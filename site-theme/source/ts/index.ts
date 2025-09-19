b1462cb58d8b17a876601c5e81c934b3932808ed
Panagiotis Konstantellos <ntellos13@hotmail.com>
2025-09-19 23:40:57 +0300

    Re-implements site theme initialization
    Re-implements the initialization logic for the site theme.
    
    The previous implementation included placeholder code and an
    incorrect module registration. This change corrects the module
    registration and ensures proper initialization of the theme.

--- site-theme/source/ts/index.ts
+++ site-theme/source/ts/index.ts
@@ -1,74 +1,13 @@
 import { WebHostBuilder } from '@bytethat/core';
-import menuModule from 'packages/theme/main';
+import themeModule from 'packages/theme/main';
 
 import $ from 'jquery';
 
-
-
-
-
-
-class Foo {
-    constructor() {
-    }
-
-    foo(): void {
-        console.log('Foo method called');
-    }
-}
-
-class ChildFoo extends Foo {
-    constructor() {
-        super();
-    }
-
-    foo(): void {
-        console.log('ChildFoo method called');
-    }
-}
-
-class Bar {
-    private _foo: Foo;
-
-    constructor(foo: Foo) {
-        this._foo = foo;
-    }
-
-    bar(): void {
-        this._foo.foo();
-
-        console.log('Bar method called');
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
 const system = WebHostBuilder.create()
-    .registerModule({
-        configureServices(services) {
-            services.add(Foo, () => new Foo());
-            services.add(Foo, () => new ChildFoo());
-            services.add(Bar, (sp) => new Bar(sp.get(Foo)!));
-        },
-        configure(services) {
-            const bar = services.get(Bar);
-
-            bar?.bar();
-        }
-    })
-    .registerModule(menuModule)
+    .registerModule(themeModule)
     .build();
 
-    system.initialize();
+system.initialize();
 
 $(document).ready(() => {
 
